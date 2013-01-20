a79177ad68c6864f958fe0637145b6c4a54bd7c6
Andreas Adelmann <andreas.adelmann@psi.ch>
2013-01-20 20:33:35 +0000

    add more developer

--- src/addToDoxygenMainPage.h
+++ src/addToDoxygenMainPage.h
@@ -1,13 +1,14 @@
 /*! \mainpage
 <P>
 <B>
-OPAL is a tool for charged-particle optics in particle accelerators and beam lines. It can handle very large and very small accelerators and solves various problems on such machines. OPAL is based on the CLASSIC class library,
-and the features of the former Mad9p (space charge and parallelism).  The OPAL framework makes it easy to add new features in the form of new C++ classes. This page is constantly being updated (once a day!).
+OPAL is a parallel tool for charged-particle optics in particle accelerators and beam lines with full 3D space charge. It can handle very large and very small accelerators and solves various problems on such machines. The OPAL framework makes it easy to add new features in the form of new C++ classes. This page is constantly being updated (once a day!).
 <P>
 
 <P>
-Developed by Andreas Adelmann, Christof Kraus, Yves Ineichen (PSI), Steve Russell (LANL), Yuanjie Bi and Jianjun Yang (CIAE)
-with former input from F.C. Iselin.
+Developed by Andreas Adelmann, Achim Gsell Christof Kraus, Yves Ineichen (PSI), 
+Steve Russell (LANL),\\ Yuanjie Bi, Chuan Wang, Jianjun Yang (CIAE), \\  Hao Zha (Thinghua University) \\ 
+Suzanne Sheehy, Chris Rogers (RAL) and \\ Christopher Mayes (Cornell)
+
 </B>
 <P>
 For further information contact: <a href="mailto:andreas.adelmann@psi.ch">andreas.adelmann@psi.ch</a> -
