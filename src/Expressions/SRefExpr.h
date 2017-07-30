931b5e2b04b191b5383d8e9f50f7de6607c1cf81
Christof Kraus <christof.j.kraus@gmail.com>
2017-07-30 14:58:54 +0200

    improve output on parse error

--- src/Expressions/SRefExpr.h
+++ src/Expressions/SRefExpr.h
@@ -22,7 +22,7 @@
 #include "AbstractObjects/Invalidator.h"
 #include "AbstractObjects/OpalData.h"
 #include "Expressions/SValue.h"
-#include "Utilities/OpalException.h"
+#include "Utilities/ParseError.h"
 #include "Utilities/Options.h"
 #include <iosfwd>
 #if defined(__GNUC__) && __GNUC__ < 3
@@ -126,7 +126,7 @@ namespace Expressions {
             if(SValue<T> *value = dynamic_cast<SValue<T>*>(base)) {
                 return value->evaluate();
             } else {
-                throw OpalException("SRefExpr::evaluate()", "Reference \"" +
+                throw ParseError("SRefExpr::evaluate()", "Reference \"" +
                                     getImage() + "\" is not a variable.");
             }
         }
@@ -174,10 +174,10 @@ namespace Expressions {
             itsObject = OpalData::getInstance()->find(obj_name);
             if(itsObject == 0) {
                 if(att_name.empty()) {
-                    throw OpalException("SRefExpr::fill()",
+                    throw ParseError("SRefExpr::fill()",
                                         "\nThe <variable> \"" + obj_name + "\" is unknown.\n");
                 } else {
-                    throw OpalException("SRefExpr::fill()",
+                    throw ParseError("SRefExpr::fill()",
                                         "Object \"" + obj_name + "\" is unknown.");
                 }
             }
@@ -189,13 +189,13 @@ namespace Expressions {
             if(att_name.empty()) {
                 itsAttr = itsObject->findAttribute("VALUE");
                 if(itsAttr == 0) {
-                    throw OpalException("SRefExpr::fill()", "Object \"" + obj_name +
+                    throw ParseError("SRefExpr::fill()", "Object \"" + obj_name +
                                         "\" is not a variable, constant or vector.");
                 }
             } else {
                 itsAttr = itsObject->findAttribute(att_name);
                 if(itsAttr == 0) {
-                    throw OpalException("SRefExpr::fill()", "Attribute \"" + obj_name +
+                    throw ParseError("SRefExpr::fill()", "Attribute \"" + obj_name +
                                         "->" + att_name + "\" is unknown.");
                 }
             }
--- src/OpalParser/OpalParser.cpp
+++ src/OpalParser/OpalParser.cpp
@@ -592,84 +592,84 @@ Statement *OpalParser::readStatement(TokenStream *is) const {
 
 void OpalParser::run() const {
     stopFlag = false;
-
+    Inform errorMsg("Error", std::cerr);
     while(Statement *stat = readStatement(&*inputStack.back())) {
         try {
             // The dispatch via Statement::execute() allows a special
             // treatment of structured statements.
             stat->execute(*this);
         } catch(ParseError &ex) {
-            ERRORMSG("\n*** Parse error detected by function \""
-                     << ex.where() << "\"\n");
+            errorMsg << "\n*** Parse error detected by function \""
+                     << ex.where() << "\"\n";
             stat->printWhere(*IpplInfo::Error, true);
             std::string what = ex.what();
             size_t pos = what.find_first_of('\n');
             do {
-                ERRORMSG("    " << what.substr(0, pos) << endl);
+                errorMsg << "    " << what.substr(0, pos) << endl;
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
 
 	    exit(1);
         } catch(OpalException &ex) {
-            ERRORMSG("\n*** User error detected by function \""
-                     << ex.where() << "\"\n parser");
+            errorMsg << "\n*** User error detected by function \""
+                     << ex.where() << "\"\n parser";
             stat->printWhere(*IpplInfo::Error, true);
             std::string what = ex.what();
             size_t pos = what.find_first_of('\n');
             do {
-                ERRORMSG("    " << what.substr(0, pos) << endl);
+                errorMsg << "    " << what.substr(0, pos) << endl;
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
 
             exit(1);
         } catch(ClassicException &ex) {
-            ERRORMSG("\n*** User error detected by function \""
-                     << ex.where() << "\"\n");
+            errorMsg << "\n*** User error detected by function \""
+                     << ex.where() << "\"\n";
             stat->printWhere(*IpplInfo::Error, true);
             std::string what = ex.what();
             size_t pos = what.find_first_of('\n');
             do {
-                ERRORMSG("    " << what.substr(0, pos) << endl);
+                errorMsg << "    " << what.substr(0, pos) << endl;
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
 
             exit(1);
         } catch(std::bad_alloc &) {
-            ERRORMSG("\n*** Error:\n");
+            errorMsg << "\n*** Error:\n";
             stat->printWhere(*IpplInfo::Error, false);
-            ERRORMSG("    " << *stat << "    Sorry, virtual memory exhausted.\n" << endl);
+            errorMsg << "    " << *stat << "    Sorry, virtual memory exhausted.\n" << endl;
         } catch(assertion &ex) {
-            ERRORMSG("\n*** Runtime-error ******************\n");
+            errorMsg << "\n*** Runtime-error ******************\n";
             std::string what = ex.what();
             size_t pos = what.find_first_of('\n');
             do {
-                ERRORMSG("    " << what.substr(0, pos) << endl);
+                errorMsg << "    " << what.substr(0, pos) << endl;
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
 
-            ERRORMSG("\n************************************\n" << endl);
+            errorMsg << "\n************************************\n" << endl;
             throw std::runtime_error("in Parser");
         } catch(std::exception &ex) {
-            ERRORMSG("\n*** Error:\n");
+            errorMsg << "\n*** Error:\n";
             stat->printWhere(*IpplInfo::Error, false);
-            ERRORMSG("    Internal OPAL error: ");
+            errorMsg << "    Internal OPAL error: ";
             std::string what = ex.what();
             size_t pos = what.find_first_of('\n');
             do {
-                ERRORMSG("    " << what.substr(0, pos) << endl);
+                errorMsg << "    " << what.substr(0, pos) << endl;
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
 
         } catch(...) {
-            ERRORMSG("\n*** Error:\n");
+            errorMsg << "\n*** Error:\n";
             stat->printWhere(*IpplInfo::Error, false);
 
-            ERRORMSG("    " << *stat << "    Unexpected exception caught.\n" << endl);
+            errorMsg << "    " << *stat << "    Unexpected exception caught.\n" << endl;
 	    throw std::runtime_error("in Parser");
         }
 
