450e57f0c188fab0e6d39c17de2615a809b5bbb4
Christof Metzger-Kraus <christof.j.metzger@gmail.com>
2017-12-26 21:49:51 +0100

    Opal Parser swallowed exception messages

--- src/OpalParser/OpalParser.cpp
+++ src/OpalParser/OpalParser.cpp
@@ -616,6 +616,7 @@ void OpalParser::run() const {
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
+            errorMsg << "    " << what << endl;
 
 	    exit(1);
         } catch(OpalException &ex) {
@@ -629,6 +630,7 @@ void OpalParser::run() const {
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
+            errorMsg << "    " << what << endl;
 
             exit(1);
         } catch(ClassicException &ex) {
@@ -642,6 +644,7 @@ void OpalParser::run() const {
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
+            errorMsg << "    " << what << endl;
 
             exit(1);
         } catch(std::bad_alloc &) {
@@ -657,6 +660,7 @@ void OpalParser::run() const {
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
+            errorMsg << "    " << what << endl;
 
             errorMsg << "\n************************************\n" << endl;
             throw std::runtime_error("in Parser");
@@ -671,7 +675,9 @@ void OpalParser::run() const {
                 what = what.substr(pos + 1, std::string::npos);
                 pos = what.find_first_of('\n');
             } while (pos != std::string::npos);
+            errorMsg << "    " << what << endl;
 
+            exit(1);
         } catch(...) {
             errorMsg << "\n*** Error:\n";
             stat->printWhere(*IpplInfo::Error, false);
