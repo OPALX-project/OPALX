17ee04f465c821ae75216097454de728b7f2ea1f
Alexander Liemen <alexander.liemen@psi.ch>
2024-10-07 11:00:08 +0200

    Create AdaptBins files

---
+++ AdaptBins.cpp
@@ -0,0 +1,7 @@
+
+#include "AdaptBins.h"
+
+
+
+
+
---
+++ AdaptBins.h
@@ -0,0 +1,33 @@
+/**
+ * @file AdaptBins.h
+ * @brief Defines a structure to hold particles in energy bins and their associated data.
+ * 
+ * "AdaptBins" is a improved version of "PartBins" from the old OPAL. AdaptBins uses
+ * data structures from IPPL and Kokkos to make improve performance when using MPI and Cuda.
+ * In contrast to the old PartBin, this class allows for the use of rebinning during runtime. 
+ */
+
+/**
+ * @copyright Copyright (c) 2007-2020, Paul Scherrer Institut, Villigen PSI, Switzerland
+ * All rights reserved
+ *
+ * This file is part of OPALX.
+ *
+ * OPAL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OPAL. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ADAPT_BINS_H
+#define ADAPT_BINS_H
+
+
+
+
+#endif 
+
+
