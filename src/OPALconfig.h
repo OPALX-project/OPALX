4472888cadc2581dbff906cced9076a7cce5c635
Andreas Adelmann <aaadelmann@users.noreply.github.com>
2026-03-18 01:13:21 +0100

    Update OPAL version and bug report contact

--- src/OPALconfig.h
+++ src/OPALconfig.h
@@ -2,13 +2,13 @@
 #define OPAL_PROJECT_NAME "OPALX"
 
 /* Define OPAL version */
-#define OPAL_PROJECT_VERSION "1.0"
-#define OPAL_VERSION_MAJOR   1
-#define OPAL_VERSION_MINOR   0
+#define OPAL_PROJECT_VERSION "0.5.0"
+#define OPAL_VERSION_MAJOR   0
+#define OPAL_VERSION_MINOR   5
 #define OPAL_VERSION_PATCH   0
 
 /* Define string with the options used to compile OPAL */
 #define OPAL_COMPILE_OPTIONS "-Wall"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
+#define PACKAGE_BUGREPORT "opal@lists.psi.ch"
