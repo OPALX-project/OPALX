7b63946b75b8f4c025b3097883b2f956c4b98ef8
Ryan Ammann <ryanammann24@gmail.com>
2026-06-23 11:16:21 +0200

    Remove redundant multipole field.

--- src/Elements/OpalQuadrupole.cpp
+++ src/Elements/OpalQuadrupole.cpp
@@ -68,32 +68,20 @@ void OpalQuadrupole::update() {
     double length      = getLength();
     mult->setElementLength(length);
 
-    // Build a multipole field from the scalar K1 / K1S values.
     // The KN vector convention is: index 0 = dipole, index 1 = quadrupole, ...
-    BMultipoleField field;
-
+    // Populate the device coefficient views read by Multipole::apply().
     double k1   = Attributes::getReal(itsAttr[K1]);
     double dk1  = Attributes::getReal(itsAttr[DK1]);
     double k1s  = Attributes::getReal(itsAttr[K1S]);
     double dk1s = Attributes::getReal(itsAttr[DK1S]);
 
-    double factor = OpalData::getInstance()->getP0() / Physics::c;
-
-    // comp 0 (dipole): strength = 0, factor /= 1
-    factor /= 1.0;  // comp 0
-    field.setNormalComponent(0, 0.0);
+    // comp 0 (dipole): strength = 0
     mult->setNormalComponent(0, 0.0, 0.0);
-    field.setSkewComponent(0, 0.0);
     mult->setSkewComponent(0, 0.0, 0.0);
 
-    // comp 1 (quadrupole): factor /= 2
-    factor /= 2.0;
-    field.setNormalComponent(1, k1 * factor);
+    // comp 1 (quadrupole)
     mult->setNormalComponent(1, k1, dk1);
-    field.setSkewComponent(1, k1s * factor);
     mult->setSkewComponent(1, k1s, dk1s);
-
-    mult->setField(field);
 
     // Transmit "unknown" attributes.
     OpalElement::updateUnknown(mult);
