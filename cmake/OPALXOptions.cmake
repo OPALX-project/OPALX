ef9b0b51b982ef10a8389424d5da16bbd658298a
John Biddiscombe <biddisco@cscs.ch>
2026-09-23 15:10:09 +0200

    cleanup cmake presets and passthrough of flags from opalx to ippl

--- cmake/OPALXOptions.cmake
+++ cmake/OPALXOptions.cmake
@@ -208,9 +208,9 @@ endif()
 
 set(IPPL_ENABLE_FFT     ON  CACHE BOOL "" FORCE)
 set(IPPL_ENABLE_SOLVERS ON  CACHE BOOL "" FORCE)
-set(IPPL_ENABLE_ALPINE  OFF CACHE BOOL "" FORCE)
-set(IPPL_ENABLE_TESTS   OFF CACHE BOOL "" FORCE)
 set(IPPL_PLATFORMS "${OPALX_PLATFORMS}" CACHE STRING "" FORCE)
+#set(IPPL_ENABLE_ALPINE  OFF CACHE BOOL "" FORCE)
+#set(IPPL_ENABLE_TESTS   OFF CACHE BOOL "" FORCE)
 
 # -----------------------------------------------------------------------------
 # Other OPALX options
